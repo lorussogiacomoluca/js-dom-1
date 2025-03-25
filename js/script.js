9987fbacc75b89328821de8bbca5c31d20cdf67e
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-25 16:30:52 +0100

    [UPD] addEventListener

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@
 const lamp = document.querySelector('img');
 const btn = document.querySelector('.btn')
 
+//2. addEventListener
+
+btn.addEventListener('click', function(){
+    console.log((!lamp.src.includes('yellow')) ? lamp.src = lamp.src.replace('white','yellow') : lamp.src = lamp.src.replace('yellow','white'));
+})
