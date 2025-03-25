c3995ff2474f8ecba357337023a252cdafb99e25
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-25 16:47:51 +0100

    [BONUS] addEventListener for toogleBtn

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -11,8 +11,23 @@ function toggleLamp(){
     }
 }
 
-//3. addEventListener
+//3. addEventListener for toggleLamp
 btn.addEventListener('click', toggleLamp)
 
-//4. Toggle Button
+//4. Toggle Button function
 console.log(btn);
+
+function toggleBtn(){
+    if (btn.textContent == 'Accendi'){
+        btn.textContent = 'Spegni'
+        btn.classList.remove('btn-primary');
+        btn.classList.add('btn-danger');
+    }else{
+        btn.textContent = 'Accendi';
+        btn.classList.remove('btn-danger');
+        btn.classList.add('btn-primary');
+    }
+}
+
+//5. addEventListener for toggleBtn
+btn.addEventListener('click', toggleBtn)
